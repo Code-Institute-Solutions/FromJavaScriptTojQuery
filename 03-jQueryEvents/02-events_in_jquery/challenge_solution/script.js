3f74867f724843e1a247c5cd5be8f7784f2d147b
NielMc <nielmce@gmail.com>
2020-01-06 12:22:11 +0000

    simplify jQuery solution

--- 03-jQueryEvents/02-events_in_jquery/challenge_solution/script.js
+++ 03-jQueryEvents/02-events_in_jquery/challenge_solution/script.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 
     //applies colour red to paragraphs when clicked on 
     $("p").click(function(){
-        $("p").addClass("highlight_text");
+        $("p").css('color', 'red');
     });
 
     //will add lightblue to h2 elements
     $("h2").hover(function(){
-        $("h2").addClass( "h2_color");   
+        $("h2").css('background-color', 'lightblue'); 
     });
 
     /*
@@ -17,58 +17,57 @@ $(document).ready(function(){
     not the other h2 elements by removing class h2_font_size from them
     */
     $("#hr_html").hover(function(){
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_python").removeClass("h2_font_size");
-        $("#hr_jquery").removeClass("h2_font_size");
-        $("#hr_django").removeClass("h2_font_size");
-        $("#hr_css").removeClass("h2_font_size");
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_html").addClass("h2_font_size");
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '2em');
     });
 
     $("#hr_mysql").hover(function(){
-        $("#hr_python").removeClass("h2_font_size");
-        $("#hr_jquery").removeClass("h2_font_size");
-        $("#hr_django").removeClass("h2_font_size");
-        $("#hr_css").removeClass("h2_font_size");
-        $("#hr_html").removeClass("h2_font_size");
-        $("#hr_mysql").addClass("h2_font_size");
+        $("#hr_mysql").css('font-size', '2em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
     });
 
     $("#hr_python").hover(function(){
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_jquery").removeClass("h2_font_size");
-        $("#hr_django").removeClass("h2_font_size");
-        $("#hr_css").removeClass("h2_font_size");
-        $("#hr_html").removeClass("h2_font_size");
-        $("#hr_python").addClass("h2_font_size");
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '2em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
     });
 
     $("#hr_jquery").hover(function(){
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_python").removeClass("h2_font_size");
-        $("#hr_django").removeClass("h2_font_size");
-        $("#hr_css").removeClass("h2_font_size");
-        $("#hr_html").removeClass("h2_font_size");
-        $("#hr_jquery").addClass("h2_font_size");
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '2em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
     });
 
     $("#hr_django").hover(function(){
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_python").removeClass("h2_font_size");
-        $("#hr_jquery").removeClass("h2_font_size");
-        $("#hr_css").removeClass("h2_font_size");
-        $("#hr_html").removeClass("h2_font_size");
-        $("#hr_django").addClass("h2_font_size");
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '2em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '1em');
     });
 
     $("#hr_css").hover(function(){
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_python").removeClass("h2_font_size");
-        $("#hr_jquery").removeClass("h2_font_size");
-        $("#hr_django").removeClass("h2_font_size");
-        $("#hr_html").removeClass("h2_font_size");
-        $("#hr_css").addClass("h2_font_size");
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '2em');
+        $("#hr_html").css('font-size', '1em');
     });
 
     //applies colour black to body background when mouse enters over buttons

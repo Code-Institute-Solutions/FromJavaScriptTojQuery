4d543b94640a99ea0fe67619d76db611610d7e7a
NielMc <nielmce@gmail.com>
2020-01-08 14:49:00 +0000

    add solution code for button.html

--- 03-jQueryEvents/07-method_chaining/challenge_solution/Sample-Site/js/script.js
+++ 03-jQueryEvents/07-method_chaining/challenge_solution/Sample-Site/js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
  //
  // add your jQuery code here
+$("#button1").mouseenter(function(){
+$('#button1').removeClass("makeRed").addClass("makeBorder");
+});
 
+$("#button1").mouseleave(function(){
+$("#button1").removeClass("makeBorder").addClass("makeRed");
+});
 
 }); 

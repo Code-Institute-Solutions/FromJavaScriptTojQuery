225d08d797bb4478b26c529feee7e8b525bf1b6c
NielMc <nielmce@gmail.com>
2020-01-10 10:52:05 +0000

    Solution code for button method chaining

--- 03-jQueryEvents/09-method_chaining_challenge_3/Sample-Site/js/script.js
+++ 03-jQueryEvents/09-method_chaining_challenge_3/Sample-Site/js/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
  //
  // add your jQuery code here
+	// hides/shows paragraphs when either button is clicked
+	$("button").click(function() {
+		$("p").hide(2000).show(2000);
+	});
 
+	$("button").click(function(){
+		$("p").fadeIn().fadeOut();
+	});
 
 }); 
